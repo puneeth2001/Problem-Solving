609924c0a834d0882c8369a16f46cecacf0fb6cf
Puneeth Chanda <puneethchanda2001@gmail.com>
2023-03-24 09:35:28 +0530

    dd all files

--- a20j/Main.java
+++ a20j/Main.java
@@ -5,26 +5,11 @@ public class Main
 public static void main(String[] args)  
     {  
         Scanner scn = new Scanner(System.in); //System.in is a standard input stream  
-        int n = scn.nextInt();              //reads string   
-        int[] intArray = new int[3*n];
-        int p =0;int q=0;int r=0;
-        for(int i = 0;i<n*3;i++){
-            int val = scn.nextInt();
-            intArray[i] = val;
-        }
-    
-        for(int i = 0;i<n*3;i+=3){
-            p = p+intArray[i];
-            q = q+intArray[i+1];
-            r = r+intArray[i+2];
-        }
+        int a = scn.nextInt();              //reads string   
+        int b = scn.nextInt();
+        int c = scn.nextInt();
 
-        if(p==0 && q==0 && r==0){
-            System.out.println("YES");
-        }
-        else{
-            System.out.println("NO");
-        }
+        if(a)
 
     }  
 }  
